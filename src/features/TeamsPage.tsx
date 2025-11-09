3d5326e0fb5f34ed181981fa2668fc7dbf7746d0
Alen Rajher <alen.rajher@gmail.com>
2025-11-09 22:03:57 +0100

    build

--- src/features/TeamsPage.tsx
+++ src/features/TeamsPage.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { supabase } from '../lib/supabaseClient';
-import { Pencil, Plus, Trash } from 'lucide-react';
+import { Pencil, Trash } from 'lucide-react';
 import classNames from 'classnames';
 
 interface Team {
